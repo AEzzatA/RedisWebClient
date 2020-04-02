2790a29a35b2c005054831acafd56cb22c2f4c14
Ahmed Kotb <ak@greentec-development.com>
2020-04-02 22:48:01 +0200

    Basic CRUD

--- webapp/models.py
+++ webapp/models.py
@@ -1,20 +1,21 @@
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 from django.urls import reverse
+from django.utils.text import slugify
 
 
 class RedisServer(models.Model):
     name = models.CharField(
-        max_length=255, help_text=_("A name to identify the server")
+        max_length=255, unique=True, help_text=_("A name to identify the server")
     )
-
     slug = models.SlugField(max_length=255, unique=True, allow_unicode=True)
-    host = models.CharField(max_length=255)
-    port = models.CharField(max_length=4)
-    password = models.PositiveIntegerField()
+    host = models.CharField(max_length=255, help_text=_("redis server host"))
+    port = models.PositiveIntegerField(max_length=4, help_text=_("redis server port"))
+    password = models.CharField(max_length=255, help_text=_("redis server host"))
+    db = models.PositiveIntegerField()
 
     def get_absolute_url(self):
         return reverse("server-info", kwargs={"pk": self.pk})
 
-    def __str__(self):
-        return name
+    def __str__(self) -> str:
+        return self.name
--- webapp/views.py
+++ webapp/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from typing import List
 
-# Create your views here.
+from django.urls import reverse_lazy
+from django.views.generic.edit import CreateView, DeleteView, UpdateView
+
+from webapp.models import RedisServer
+
+CRUD_FIELDS: List[str] = ["name", "host", "password", "port", "db"]
+
+
+class ServerCreate(CreateView):
+    model = RedisServer
+    fields = CRUD_FIELDS
+
+
+class ServerUpdate(UpdateView):
+    model = RedisServer
+    fields = CRUD_FIELDS
+    template_name_suffix = "_update_form"
+
+
+class ServerDelete(DeleteView):
+    model = RedisServer
+    success_url = reverse_lazy("home-page")
